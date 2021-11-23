96a6736f702dc73dc53ef587f12b8132a0c6b275
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-23 14:21:54 -0500

    Initial

---
+++ PhyloSeq2QiimePlugin.R
@@ -0,0 +1,32 @@
+library(microbiome)
+library(biomformat)
+library(ape)
+
+
+
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+input <- function(inputfile) {
+  pfix = prefix()
+
+  parameters <- read.table(inputfile, as.is=T);
+  rownames(parameters) <- parameters[,1]
+  print(parameters["OTU", 2])
+  otu_file <- paste(pfix, toString(parameters["OTU", 2]), sep="/")
+  tax_file <- paste(pfix, toString(parameters["TAX", 2]), sep="/")
+  sample_file <- paste(pfix, toString(parameters["META", 2]), sep="/")
+  p0 <<- read_csv2phyloseq(otu.file = otu_file, taxonomy.file = tax_file, metadata.file = sample_file, sep=",")
+}
+
+run <- function() {
+  m0 <<- make_biom(data=data.frame(otu_table(p0)), sample_metadata=data.frame(sample_data(p0)), observation_metadata=data.frame(tax_table(p0)))
+}
+
+output <- function(outputfile) {
+write_biom(m0, paste(outputfile, "biom", sep="."))#"mystuff.biom")
+random_tree = rtree(ntaxa(p0), rooted=TRUE, tip.label=taxa_names(p0))
+p1 = merge_phyloseq(p0, random_tree)
+write.tree(phy_tree(p1), paste(outputfile, "tre", sep="."), tree.names=TRUE)
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # PhyloSeq2Qiime
-Convert PhyloSeq input files (OTU, TAX, META) to Qiime (BIOM, TRE)
+# Language: R
+# Input: TXT (keyword-value pairs)
+# Output: PREFIX
+# Tested with: PluMA 1.1, R 4.0.0
+# Dependency: ape_5.4-1, biomformat_1.18.0, microbiome_1.12.0, ggplot2_3.3.5, phyloseq_1.34.0
+
+PluMA plugin to convert PhyloSeq-compatible input data (OTU, TAX and META files)
+into Qiime-compatible BIOM and TRE format.
+
+The plugin accepts as input a tab-delimited parameter file of keyword value pairs:
+OTU: OTU table
+TAX: TAX table
+META: metadata
+
+Output will be files PREFIX.biom and PREFIX.tre.
